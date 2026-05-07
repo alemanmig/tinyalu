d19326ef1397b4f6c6e9aca533309b58d18711b8
dlopezm2025-cmyk <dlopezm2025@cic.ipn.mx>
2026-05-07 08:42:20 -0700

    Simulacion corregida

--- rtl/one_pkg.sv
+++ rtl/one_pkg.sv
@@ -5,6 +5,7 @@ typedef enum bit [2:0] {no_op   = 3'b000,
                         and_op  = 3'b010,
                         xor_op  = 3'b011,
                         mul_op  = 3'b100,
-                        rst_op  = 3'b111  } operation_t;
+                        rst_op  = 3'b111  
+    } operation_t;
 
 endpackage : one_pkg
--- verification/directed/tests/test.sv
+++ verification/directed/tests/test.sv
@@ -4,11 +4,24 @@ module test (
   vif_if vif
 );
 
+  operation_t op_set;
+
+  assign vif.op_i = op_set;
+
   covergroup op_conv;
   
     coverpoint vif.op_set {
       bins single_cycle[] = {[add_op : xor_op], rst_op, no_op};
       bins multi_cycle    = {mul_op};
+
+      bins opn_rst[] = ([add_op:mul_op] => rst_op);
+      bins rst_opn[] = (rst_op => [add_op:mul_op]);
+
+      bins sngl_mul[] = ([add_op:xor_op], no_op => mul_op);
+      bins mul_sngl[] = (mul_op => [add_op:xor_op], no_op);
+
+      bins twoops[] = ([add_op:mul_op] [* 2]);
+      bins manymult[] = (mul_op [* 3:5]);
     }
   endgroup
 
@@ -23,87 +36,89 @@ module test (
       bins others = {[8'h01:8'hFE]};
       bins ones   = {8'hFF};
     }
+
+    b_leg: coverpoint vif.B {
+      bins zeros = {'h00};
+      bins others= {['h01:'hFE]};
+      bins ones  = {'hFF};
+      }
+
+     op_00_FF:  cross a_leg, b_leg, all_ops {
+         bins add_00 = binsof (all_ops) intersect {add_op} &&
+                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+         bins add_FF = binsof (all_ops) intersect {add_op} &&
+                       (binsof (a_leg.ones) || binsof (b_leg.ones));
+
+         bins and_00 = binsof (all_ops) intersect {and_op} &&
+                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+         bins and_FF = binsof (all_ops) intersect {and_op} &&
+                       (binsof (a_leg.ones) || binsof (b_leg.ones));
+
+         bins xor_00 = binsof (all_ops) intersect {xor_op} &&
+                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+         bins xor_FF = binsof (all_ops) intersect {xor_op} &&
+                       (binsof (a_leg.ones) || binsof (b_leg.ones));
+
+         bins mul_00 = binsof (all_ops) intersect {mul_op} &&
+                       (binsof (a_leg.zeros) || binsof (b_leg.zeros));
+
+         bins mul_FF = binsof (all_ops) intersect {mul_op} &&
+                       (binsof (a_leg.ones) || binsof (b_leg.ones));
+
+         bins mul_max = binsof (all_ops) intersect {mul_op} &&
+                        (binsof (a_leg.ones) && binsof (b_leg.ones));
+
+         ignore_bins others_only =
+                                  binsof(a_leg.others) && binsof(b_leg.others);
+
+      }
+
   endgroup
-
-function automatic logic [7:0] get_data();
-  get_data = $urandom_range(8'h00, 8'hFF);
-endfunction : get_data
-
-function automatic operation_t get_op();
-  int unsigned sel;
-
-  sel = $urandom_range(0, 5);
-
-  case (sel)
-    0: get_op = no_op;
-    1: get_op = add_op;
-    2: get_op = and_op;
-    3: get_op = xor_op;
-    4: get_op = mul_op;
-    5: get_op = rst_op;
-    default: get_op = no_op;
-  endcase
-endfunction : get_op
-
 
   op_conv              oc;
   zeros_or_ones_on_ops c_00_FF;
+  
   shortint predicted_result;
 
   initial begin : coverage
     oc     = new();
     c_00_FF = new();
 
-    forever begin
-      @(negedge vif.clk_i);
+    forever begin @(negedge vif.clk_i);
       oc.sample();
       c_00_FF.sample();
     end
-  end
+  end: coverage
+
+  ///////////////////////////////////////////////////////////
+  //    Main sequence
 
   initial begin
     $display("Begin Of Simulation.");
-
-    vif.A      = '0;
-    vif.B      = '0;
-    vif.op_i   = '0;
-    vif.start  = 1'b0;
+    vif.rst_ni = 1'b0;
+    @(negedge vif.clk_i);
+    @(negedge vif.clk_i);
     vif.rst_ni = 1'b1;
+    vif.start = 1'b1;
 
-  fork
-    tester();
-    scoreboard(); 
-  join
+    fork
+        scoreboard(); 
+    join_none
 
     #(200ns);
     $display("End Of Simulation.");
     $finish;
   end
 
-  task automatic tester();
-    vif.rst_ni = 1'b0;
-    vif.start  = 1'b0;
-    @(negedge vif.clk_i);
-    @(negedge vif.clk_i);
-    vif.rst_ni = 1'b1;
-
-    repeat (1000) begin
-      @(negedge vif.clk_i);
-      vif.op_set = get_op();
-      vif.op_i   = vif.op_set;
-      vif.A      = get_data();
-      vif.B      = get_data();
-
-      vif.start = 1'b1;
-      @(negedge vif.clk_i);
-      vif.start = 1'b0;
-    end
-  endtask
-
 task automatic scoreboard();
   byte        A_s, B_s;
   operation_t op_s;
   shortint    predicted_result;
+
+  wait(vif.rst_ni == 1'b1);
 
   forever begin
     @(posedge vif.start);
